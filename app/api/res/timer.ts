80766ac5d199ff6a809e77e3088ed1d9ed8cd232
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2025-03-06 14:47:23 -0300

    Create resource route to produce regular events

--- app/api/res/timer.ts
+++ app/api/res/timer.ts
@@ -1,3 +1,15 @@
-export const loader = () => {
-  return null
+import type { LoaderFunctionArgs } from "@remix-run/node"
+
+import { eventStream } from "remix-utils/sse/server"
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return eventStream(request.signal, send => {
+    const timer = setInterval(() => {
+      send({ event: "iso8601-timedate", data: new Date().toISOString() })
+    }, Math.random() * 1000)
+
+    const clear = () => clearInterval(timer)
+
+    return clear
+  })
 }
